dc22e5d8eb7a8b612903280155fde4551ba8d543
Collins0399 <kiprotichcollince34@gmail.com>
2026-07-21 12:30:57 +0300

    Configure Vercel API proxy rewrites for HTTPS login support

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      '/api': {
+        target: 'http://51.15.243.105:30746',
+        changeOrigin: true,
+      }
+    },
     allowedHosts: [
       '1854-105-164-128-20.ngrok-free.app'
     ]
